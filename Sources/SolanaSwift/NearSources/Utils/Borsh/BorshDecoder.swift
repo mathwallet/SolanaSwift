5d829517bc7e36d72662d1c04c9f45536eae0657
xueyuejie <358838943@qq.com>
2021-11-17 15:52:42 +0800

    fix

--- Sources/SolanaSwift/NearSources/Utils/Borsh/BorshDecoder.swift
+++ Sources/SolanaSwift/NearSources/Utils/Borsh/BorshDecoder.swift
@@ -8,12 +8,8 @@
 import Foundation
 
 public struct BorshDecoder {
-//    public func decode<T>(_ type: T.Type, from data: Data) throws -> T where T : BorshDeserializable {
-//        var reader = BinaryReader(bytes: [UInt8](data))
-//        return try T.init(from: &reader)
-//    }
-    public func decode<T>(_ type: T.Type, from data: Data)  {
-//        var reader = BinaryReader(bytes: [UInt8](data))
-//        return try T.init(from: &reader)
+    public static func decode<T>(_ type: T.Type, from data: Data) throws -> T where T : BorshDeserializable {
+        var reader = BinaryReader(bytes: [UInt8](data))
+        return try T.init(from: &reader)
     }
 }
