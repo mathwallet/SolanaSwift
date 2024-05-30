248e9414972c8e5d19607f737f814d871887e14a
Forrest <zyyu1990@gmail.com>
2024-05-30 18:55:35 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
             targets: ["SolanaSwift"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.4.3"),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.4.1"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.9.1"),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.4.2"),
         .package(name: "Secp256k1Swift", url: "https://github.com/mathwallet/Secp256k1Swift.git", from: "2.0.0"),
         .package(url: "https://github.com/mathwallet/Base58Swift.git", from: "0.0.1"),
         .package(name: "TweetNacl", url: "https://github.com/lishuailibertine/tweetnacl-swiftwrap", from: "1.0.5"),
